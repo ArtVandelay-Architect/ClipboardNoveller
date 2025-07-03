3bab3a8c9e25e66a62384dadf163ce51744ceeff
Luke Li <zixinlukeli@gmail.com>
2025-07-02 21:27:09 -0400

    step version number

--- ClipBoardNoveller.js
+++ ClipBoardNoveller.js
@@ -1,5 +1,5 @@
 // ClipBoardNoveller for Scriptable by Luke Li
-// v1.0.17
+// v1.0.18
 
 // This program allows you to save novels you copied from your clipboard in pages,
 // each page is a different text file, and the json file contains the list of main
